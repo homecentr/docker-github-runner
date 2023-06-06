e890301b118bb1bb8e1d173b330a79001baaaff2
LH <me@lholota.com>
2023-06-06 11:13:13 +0200

    feat: Dynamic root directory to workaround clashing docker labels (#13)

--- start.sh
+++ start.sh
@@ -3,19 +3,26 @@
 GH_OWNER=$GH_OWNER
 GH_TOKEN=$GH_TOKEN
 
-CONTAINER_ID=$(head -1 /proc/self/cgroup | cut -d/ -f3 | cut -c1-5)
-RUNNER_NAME="$RUNNER_NAME-$CONTAINER_ID"
+export CONTAINER_ID=$(head -1 /proc/self/cgroup | cut -d/ -f3 | cut -c1-5)
+export RUNNER_NAME="${RUNNER_NAME}-${CONTAINER_ID}"
+
+echo "Container ID: $CONTAINER_ID"
+
+# Create a root directory symlink to get a different Docker instance id hash
+ROOT_DIR="/var/lib/github-runner-instance/$CONTAINER_ID"
+ln -s "$PWD" "$ROOT_DIR"
+cd "$ROOT_DIR"
 
 echo "Connecting to GitHub org: $GH_OWNER"
 echo "Runner name: $RUNNER_NAME"
 
 REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/orgs/${GH_OWNER}/actions/runners/registration-token | jq .token --raw-output)
 
-./config.sh --unattended --url https://github.com/${GH_OWNER} --token ${REG_TOKEN} --name ${RUNNER_NAME}
+./config.sh --unattended --url "https://github.com/${GH_OWNER}" --token "${REG_TOKEN}" --name "${RUNNER_NAME}"
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN}
+    ./config.sh remove --unattended --token "${REG_TOKEN}"
 }
 
 trap 'cleanup; exit 130' INT
